dfe33ff8353fb32ab849fc5ac3c9875d4d5806c2
Eunbin Park <cosmos.42@icloud.com>
2023-02-17 00:54:14 +0900

    Add log to database function

--- main.py
+++ main.py
@@ -1,71 +1,101 @@
 import re
 import os
+import ast
+import json
+import datetime
 from dotenv import load_dotenv
 
 from slack import RTMClient
 from chatgpt import ChatGPT
 
+from util.postgre import sql 
+
 
 load_dotenv()
 
-# 발급받은 슬랙 bot user token 기
-bot_token = os.environ["SLACK_BOT_TOKEN"]
-BOT_ID = os.environ["BOT_ID"]
-bot_id_regex = r"\<\@[A-Z0-9]+\>\s*"
 
-
-
-def chatgpt_to_slack(text, channel_id, message_ts):
-    response = ChatGPT(text)
-    # 슬랙에 메세지 전달
-    web_client.chat_postMessage(channel=channel_id, text=response, thread_ts=message_ts)
-
+def toss_to_db(log_history, bot_id, table_name: str="messages"):
+    postgres = sql()
     
-def remove_user_id_in_text(text):
-    global bot_id_regex
+    if "blocks" in log_history.keys():
+        unix_to_timestamp = datetime.datetime.fromtimestamp(float(log_history["ts"]))
+        unix_to_timestamp = datetime.datetime.strftime(unix_to_timestamp, "%Y-%m-%d %H:%M:%S.%f")
+
+        log_json = json.dumps(log_history, ensure_ascii=False).replace("'", "''")
+        
+        slack_id = ""
+            
+        try:
+            slack_id = log_history["user"]
+            
+        except:
+            slack_id = bot_id
+
+        if slack_id:
+            log_data = {"thread_id": log_history["thread_ts"] if "thread_ts" in log_history.keys() else log_history["event_ts"],
+                        "block_id": log_history["blocks"][0]["block_id"],
+                        "slack_user_id": slack_id,
+                        "conversation": log_history["text"],
+                        "created_at": unix_to_timestamp,}
+
+            values = ", ".join([f"'{i}'" for i in log_data.values()])
+            insert_format = f"INSERT INTO {table_name} VALUES ({values}, '{log_json}');"
+            postgres.insert(insert_format)
+
+
+
+def remove_user_id_in_text(text, bot_id_regex):
     return re.sub(bot_id_regex, "", text).strip()
 
 
-def extract_bot_id_in_text(text):
-    global bot_id_regex
-    global BOT_ID
-    extracted_id = re.findall(bot_id_regex, text)[0].strip()
+def check_users_bot_calling(text, bot_id_regex, bot_id):
+    extracted_id = re.findall(bot_id_regex, text)
+    result = False
     
-    return True if extracted_id == f"<@{BOT_ID}>" else False
-    
+    if extracted_id:
+        if extracted_id[0].strip() == f"<@{bot_id}>":
+            result = True
+            
+    return result
+
 
 # 지속적으로 슬랙 메세지 트래킹
 @RTMClient.run_on(event="message")
-def chatgptbot(**payload):    
-    data = payload["data"]
+def chatgptbot(**payload):
+    bot_id_regex = r"\<\@[A-Z0-9]+\>\s*"
+
+    data: dict = payload["data"]
     web_client = payload["web_client"]
     
-    bot_id = data.get("bot_id", "")
+    bot_id = os.environ["BOT_ID"]
+    bot_id_in_slack_log = data.get("bot_id", "")
+    
     subtype = data.get("subtype", "")
     origin_text = data.get("text", "")
     
-    # tag_code = origin_text.split(" ")[0]
-    print(data)
     # 유저가 봇을 호출한 경우인지 확인 
-    is_this_bot_calling = extract_bot_id_in_text(origin_text)
-
+    is_this_bot_calling = check_users_bot_calling(origin_text, bot_id_regex, bot_id)
+    
+    # 슬랙 메시지 확인
+    print(data)
+    
+    if subtype == "bot_message" or is_this_bot_calling:
+        toss_to_db(data, bot_id)
     # Bot이 입력한 채팅이 아닐 경우 ChatGPT 동작
     
-    # bot_id: bot의 답장인 경우 값이 할당됨
-    # subtype: bot의 답장인 경우- "bot_message", 
-    # subtype: 사용자의 첫 스레드 답글일 경우 - "message_replied"
-    
-    if bot_id == "" and subtype == "" and is_this_bot_calling:
+    if bot_id_in_slack_log == "" and subtype == "" and is_this_bot_calling:
         # Extracting message send by the user on the slack
-        text = remove_user_id_in_text(origin_text)
+        text = remove_user_id_in_text(origin_text, bot_id_regex)
         
         # thread_ts: 해당 메세지 입력 시간을 파악하여 답글을 달 수 있도록 지정
-        #받아온 텍스트를 ChatGPT에 전달하고 ChatGPT의 답변 저장
+
         response = ChatGPT(text)
         web_client.chat_postMessage(channel=data["channel"], text=response, thread_ts=data["ts"])
         
 
+        
 if __name__ == "__main__":
+    bot_token = os.environ["SLACK_BOT_TOKEN"]
     try:
         # RTM 클라이언트 호출 및 시작
         rtm_client = RTMClient(token=bot_token)
