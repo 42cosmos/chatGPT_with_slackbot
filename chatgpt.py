f60cb3e4cd1a82b7b677930d9294e6eb0cc8175b
Eunbin Park <cosmos.42@icloud.com>
2023-02-19 01:00:56 +0900

    Modify return value to None

--- chatgpt.py
+++ chatgpt.py
@@ -11,13 +11,16 @@ load_dotenv()
 with open(os.environ["CONFIG_FILE_PATH"]) as f:
     chatgpt_config = json.load(f)
 
-def ChatGPT(prompt):
+def ChatGPT(prompt, conversation_id=None, parent_id=None):
     chatbot = Chatbot(config=chatgpt_config)
     response = {"message": "ChatGPT is at capacity right now",
-               "conversation_id": "",
-               "parent_id": ""}
+               "conversation_id": None,
+               "parent_id": None}
     try:
-        res = list(chatbot.ask(prompt))
+        res = list(chatbot.ask(prompt, 
+                               conversation_id=conversation_id, 
+                               parent_id=parent_id))
+        
         if isinstance(res, list) and res:
             response = res[-1]
     except:
